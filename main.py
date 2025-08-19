a250557dac6446ca3ab8e763feb05dc1b54dd196
Kyle Beasley <kyle.beasley17@gmail.com>
2025-08-19 00:52:17 -0500

    done for the night

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from escpos.printer import Usb
 
 p = Usb(0x0519, 0x0001, 0, profile="TSP600")
 
-p.textln("hello my love")
-p.barcode("8675308")
+p.textln("this is some text")
+p.barcode("8675309", "CODE39", width=3)
+p.qr(123456789)
 p.cut()
